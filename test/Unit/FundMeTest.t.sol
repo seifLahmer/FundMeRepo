126e7af364ceb5e18e548515df3e304298e6b9ad
seif <saiflahmar1008@gmail.com>
2024-12-29 13:44:02 +0100

    tuple type return for deploy script

--- test/Unit/FundMeTest.t.sol
+++ test/Unit/FundMeTest.t.sol
@@ -21,7 +21,7 @@ contract FundMeTest is Test {
         //fundme = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
 
         DeployFundMe deployFundMe = new DeployFundMe();
-        fundme = deployFundMe.run();
+        (fundme,) = deployFundMe.run();
         vm.deal(USER, STARTING_BALANCE);
     }
 
